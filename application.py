4c7f0584d7486179e24f93d3eb568f3714fce4c9
zhongh3 <zhon0017@gmail.com>
2018-04-02 20:24:56 +0800

    implement load_data.py using numpy arrays

--- application.py
+++ application.py
@@ -1,14 +1,16 @@
-import loaddata
+from load_data import load_data
 from model import to_model
 from train import train
 
 from evaluation import evaluation
 
+
 def main():
-    x_train, y_train, x_test, y_test = loaddata()
+    x_train, y_train, x_test, y_test = load_data()
     model = to_model(x_train)
     train(model, x_train, y_train, x_test, y_test)
     evaluation(model, x_test, y_test)
+
 
 if __name__ == '__main__':
     main()
--- loaddata.py
+++
@@ -1,2 +0,0 @@
-def loaddata():
-    raise NotImplementedError
