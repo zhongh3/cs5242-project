41ebb01ced578ed1745f88d565a2d858ad157e39
zhongh3 <zhon0017@gmail.com>
2018-04-07 22:40:26 +0800

    change config parameters to train for 96x96

--- setting.py
+++ setting.py
@@ -2,13 +2,13 @@ import os
 
 # Training Parameters
 learning_rate = 1e-4
-num_steps = 10
+num_steps = None
 batch_size = 128
-train_epoch = 5
+train_epoch = 30
 
 # Network Parameters
 num_classes = 2 # total classes (0 or 1)
-dropout = 0.25 # Dropout, probability to drop a unit
+dropout = 0.5 # Dropout, probability to drop a unit
 
 # sample length to read = in_height x in_width, the rest will be truncated
 # e.g. 64 x 64 = 4096 bytes per sample
@@ -17,7 +17,7 @@ in_width = 96
 
 # train_ratio = 0.8 --> split data as 80% training, 20% testing
 train_ratio = 0.8
-num_rows = 100
+num_rows = None
 
-max_ckpt = 160
-ckpt_steps = 200
+max_ckpt = 500
+ckpt_steps = 100
