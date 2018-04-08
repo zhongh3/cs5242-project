bc21d0e38cb47566de0b36530b3b4f0c5c734e5a
zhongh3 <zhon0017@gmail.com>
2018-04-08 10:09:11 +0800

    chang ckpt settings to avoid out of space error in NSCC

--- setting.py
+++ setting.py
@@ -4,11 +4,11 @@ import os
 learning_rate = 1e-4
 num_steps = None
 batch_size = 128
-train_epoch = 30
+train_epoch = 20
 
 # Network Parameters
-num_classes = 2 # total classes (0 or 1)
-dropout = 0.5 # Dropout, probability to drop a unit
+num_classes = 2  # total classes (0 or 1)
+dropout = 0.5  # Dropout, probability to drop a unit
 
 # sample length to read = in_height x in_width, the rest will be truncated
 # e.g. 64 x 64 = 4096 bytes per sample
@@ -19,5 +19,5 @@ in_width = 96
 train_ratio = 0.8
 num_rows = None
 
-max_ckpt = 500
-ckpt_steps = 100
+max_ckpt = 80
+ckpt_steps = 400
